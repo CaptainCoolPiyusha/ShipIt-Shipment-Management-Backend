a9cbfbac39a936402364fbfd32cf43a0e5c50f7f
Piyusha Patil <piyushapatil9585@gmail.com>
2026-09-09 16:08:03 +0530

    Fetch values from dictionary

--- app/main.py
+++ app/main.py
@@ -5,21 +5,51 @@ from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
+shipments = {
+    12403: {
+        "content": "Glasswear",
+        "weight": 0.6,
+        "status": "placed"
+    },
+    12404: {
+        "content": "Electronics",
+        "weight": 4.2,
+        "status": "in_transit"
+    },
+    12405: {
+        "content": "Books",
+        "weight": 1.8,
+        "status": "delivered"
+    },
+    12406: {
+        "content": "Furniture",
+        "weight": 22.5,
+        "status": "processing"
+    },
+    12407: {
+        "content": "Appliances",
+        "weight": 18.0,
+        "status": "placed"
+    }
+}
+
+# Order matters
+# Declare static routes before dynamic parameterized routes 
+@app.get("/shipment/latest") #static
+def get_latest_shipment():
+    id = max(shipments.keys())
+    return shipments[id]
+
 @app.get("/shipment")
 def get_shipment():
-    return {
-        "content" : "Wooden Block",
-        "status": "In transit"
-    }
+    return shipments
 
-@app.get("/shipment/{id}")
+@app.get("/shipment/{id}") #dynamic
 def get_shipment_by_ID(id: int) -> dict[str , Any]:
-    return {
-        "id": id,
-        "content": "Wooden Block",
-        "weight": 32.4,
-        "status": "In transit"
-    }
+    if id not in shipments:
+        return {"detail": "Given id doesn't exist"}
+    return shipments[id]
+
 
 @app.get("/scalar", include_in_schema=False)
 def scalar_docs():
