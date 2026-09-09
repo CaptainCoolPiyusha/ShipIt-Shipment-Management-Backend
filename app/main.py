e82d106cc5ab86cb89df20894923a699836f61c0
Piyusha Patil <piyushapatil9585@gmail.com>
2026-09-09 13:35:51 +0530

    Revise path parameter

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
@@ -10,6 +12,14 @@ def get_shipment():
         "status": "In transit"
     }
 
+@app.get("/shipment/{id}")
+def get_shipment_by_ID(id: int) -> dict[str , Any]:
+    return {
+        "id": id,
+        "content": "Wooden Block",
+        "weight": 32.4,
+        "status": "In transit"
+    }
 
 @app.get("/scalar", include_in_schema=False)
 def scalar_docs():
