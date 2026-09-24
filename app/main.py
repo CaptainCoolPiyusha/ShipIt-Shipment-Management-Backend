388569744c84041213da5ff540498b6920eea02d
Piyusha Patil <piyushapatil9585@gmail.com>
2026-09-24 16:45:51 +0530

    Revise seperation of code via modules and it's usage

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from typing import Any
 
 from fastapi import FastAPI, HTTPException, status
 from scalar_fastapi import get_scalar_api_reference
-from model import Shipment
+
+from .schemas import Shipment
+
 app = FastAPI()
 
 
