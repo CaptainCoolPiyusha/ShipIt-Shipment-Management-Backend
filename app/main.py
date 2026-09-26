724aeb66b3f521aab50cc1610332b21bc27e89a6
Piyusha Patil <piyushapatil9585@gmail.com>
2026-09-26 12:43:00 +0530

    Fetch data from json file instead of dictionary

--- app/main.py
+++ app/main.py
@@ -1,44 +1,45 @@
 from fastapi import FastAPI, HTTPException, status
 from scalar_fastapi import get_scalar_api_reference
 
+from .database import save, shipments
 from .schemas import ShipmentCreate, ShipmentRead, ShipmentUpdate
 
 app = FastAPI()
 
 
 # Shipment datastore as dict
-shipments = {
-    12403: {
-        "content": "Glassware",
-        "weight": 0.6,
-        "destination": 11000,
-        "status": "placed"
-    },
-    12404: {
-        "content": "Electronics",
-        "weight": 4.2,
-        "destination": 11001,
-        "status": "in_transit"
-    },
-    12405: {
-        "content": "Books",
-        "weight": 1.8,
-        "destination": 11002,
-        "status": "delivered"
-    },
-    12406: {
-        "content": "Furniture",
-        "weight": 22.5,
-        "destination": 11003,
-        "status": "placed"
-    },
-    12407: {
-        "content": "Appliances",
-        "weight": 18.0,
-        "destination": 11004,
-        "status": "out_for_delivery"
-    }
-}
+# shipments = {
+#     12403: {
+#         "content": "Glassware",
+#         "weight": 0.6,
+#         "destination": 11000,
+#         "status": "placed"
+#     },
+#     12404: {
+#         "content": "Electronics",
+#         "weight": 4.2,
+#         "destination": 11001,
+#         "status": "in_transit"
+#     },
+#     12405: {
+#         "content": "Books",
+#         "weight": 1.8,
+#         "destination": 11002,
+#         "status": "delivered"
+#     },
+#     12406: {
+#         "content": "Furniture",
+#         "weight": 22.5,
+#         "destination": 11003,
+#         "status": "placed"
+#     },
+#     12407: {
+#         "content": "Appliances",
+#         "weight": 18.0,
+#         "destination": 11004,
+#         "status": "out_for_delivery"
+#     }
+# }
 
 # Shipment by id
 @app.get("/shipment", response_model=ShipmentRead)
@@ -59,8 +60,10 @@ def submit_shipment(shipment: ShipmentCreate) -> dict[str, int]:
     # Add that shipment to new id
     shipments[new_id] = {
         **shipment.model_dump(),
+        "id": new_id,
         "status": "placed",
     }
+
     return {"id": new_id} # --> Will be useful later
 
 # Update fields of a shipment -> as content, weight, destination all will be same. In update we can only update status
@@ -74,7 +77,7 @@ def update_shipment(id: int, body: ShipmentUpdate):
         )
 
     shipments[id].update(body.model_dump())
-
+    save() 
     return shipments[id]
 
 # Delete a shipment
@@ -82,6 +85,7 @@ def update_shipment(id: int, body: ShipmentUpdate):
 def delete_shipment(id: int) -> dict[str, str]:
     if id in shipments:
         shipments.pop(id)
+    save()
     return{"detail": f"Shipment with id #{id} is deleted!"}
 
 
